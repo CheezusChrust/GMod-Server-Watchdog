21034c72ca82f24011eb38756d24ecbadc748344
Matt <rcheeze1337@gmail.com>
2023-04-21 16:17:11 -0400

    General cleanup
    - Removed unused sys import
    - Reload config on server restart
    - Check if .git folder exists when updating addons
    - Startup time check easier to read
    - Removed unused "monitorIP" variable

--- gmodwatchdog.py
+++ gmodwatchdog.py
@@ -5,11 +5,14 @@ import os
 from os import path
 import a2s
 import socket
-import sys
 import colorama
 from colorama import Fore as Color
 
 colorama.init()
+
+srcds = None # SRCDS process
+startupTime = 0 # Time when SRCDS was started
+lastPing = time()
 
 def warn(String):
     print(Color.RED + String + Color.RESET)
@@ -19,33 +22,15 @@ def notify(String):
     print(Color.GREEN + String + Color.RESET)
 
 
-def parseConfig(f):
-    config = {}
-
-    with open(f) as file:
-        for line in file:
-            line = line.strip()
-
-            if line.startswith("#") or len(line) == 0:
-                continue
-
-            parts = line.split("=")
-
-            if len(parts) == 2:
-                key = parts[0].strip()
-                value = parts[1].split("#")[0].strip()
-                config[key] = value
-
-    return config
-
-
 defaultConfigFile = """
 #Delete this config and start the program again to regenerate this file with default values
 
 #It's recommended to use srcds_console.exe (from Alien Swarm: Reactive Drop), because it doesn't create a second console window
 executable=srcds_console.exe
-
 args=-console -game garrysmod -console -port 27015 -norestart +maxplayers 32 +gamemode sandbox +map gm_construct
+
+#Path to SteamCMD, set if you want to update the server automatically
+steamCMDPath=
 
 #If set, ping the server every second, and if doesn't respond for [timeBeforeForceRestart] seconds, restart it
 #This should be set to your server's public IP address, with the port being the same as the one in the args
@@ -60,11 +45,37 @@ startupDelay=30
 defaultConfig = {
     "executable": "srcds_console.exe",
     "args": "-console -game garrysmod -console -port 27015 -norestart +maxplayers 16 +gamemode sandbox +map gm_construct",
+    "steamCMDPath": "",
     "monitorIP": "",
     "monitorPort": 27015,
     "timeBeforeForceRestart": 30,
     "startupDelay": 30
 }
+
+
+def loadConfig(f):
+    config = {}
+
+    with open(f) as file:
+        for line in file:
+            line = line.strip()
+
+            if line.startswith("#") or len(line) == 0:
+                continue
+
+            parts = line.split("=", 1)
+
+            if len(parts) == 2:
+                key = parts[0].strip()
+                value = parts[1].split("#")[0].strip()
+                config[key] = value
+
+    for key, value in defaultConfig.items():
+        if key not in config:
+            config[key] = value
+            warn(f"Couldn't find {key} in config file, using default value")
+
+    return config
 
 
 if not path.exists("gmodwatchdog.cfg"):
@@ -75,33 +86,14 @@ if not path.exists("gmodwatchdog.cfg"):
         exit(1)
 
 
-config = parseConfig("gmodwatchdog.cfg")
+config = loadConfig("gmodwatchdog.cfg")
 
-for key, value in defaultConfig.items():
-    if key not in config:
-        config[key] = value
-        print("Using default value for", key)
-
-
-# Can't get srcds to run properly from a location that isn't the server directory, need to figure this out sometime
-#if config["executable"] == "":
-#    config["executable"] = "srcds_console.exe"
-
-#if getattr(sys, 'frozen', False) and config["executable"] == "srcds_console.exe":
-    # we are running in a bundle
-#    executable = path.join(sys._MEIPASS, config["executable"])
-#else:
-    # we are running in a normal Python environment
 executable = path.join(os.getcwd(), config["executable"])
 
 if not path.exists(executable):
     warn(f"Executable not found, please make sure {config['executable']} exists")
     os.system("pause")
     exit(1)
-
-srcds = None
-startupTime = 0
-lastPing = time()
 
 
 def updateAddons():
@@ -119,22 +111,28 @@ def updateAddons():
     ]
 
     for d in dirs:
-        friendlyDir = d.replace(addonsDir, "")
+        if not path.exists(path.join(d, ".git")):
+            continue
+
+        addonName = d.replace(addonsDir, "")
 
         try:
-            subprocess.run(["git", "pull"], cwd=d, check=True, stderr=subprocess.DEVNULL)
-            notify(f"Successfully updated {friendlyDir}")
+            subprocess.run(["git", "pull"], cwd=d, check=True)
+            notify(f"Successfully updated {addonName}")
         except subprocess.CalledProcessError as e:
             if e.returncode == 1:
-                warn(f"Failed to update {friendlyDir}: git error")
+                warn(f"Failed to update {addonName}: git error")
         except Exception as e:
-            warn(f"Failed to update {friendlyDir}: {e}")
+            warn(f"Failed to update {addonName}: {e}")
 
 
 def runSRCDS():
     global srcds
     global startupTime
     global lastPing
+    global config
+
+    config = loadConfig("gmodwatchdog.cfg")
 
     try:
         srcds = subprocess.Popen(f"{executable} {config['args']}")
@@ -162,9 +160,6 @@ def stopServer():
         srcds = None
 
 
-monitorIP = socket.gethostbyname(socket.gethostname())
-
-
 def serverResponding():
     try:
         a2s.info((config["monitorIP"], int(config["monitorPort"])), timeout=1)
@@ -182,7 +177,10 @@ def watchdog():
     global lastPing
 
     while True:
-        runtime = time() - startupTime if startupTime != 0 else 0
+        if startupTime == 0:
+            continue
+
+        runtime = time() - startupTime
 
         if srcds is not None and runtime > int(config["startupDelay"]) and config["monitorIP"] != "":
             if serverResponding():
